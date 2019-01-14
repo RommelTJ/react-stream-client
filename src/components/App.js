67b0f8a2056b59ca4eac0cdd1ea812081a4d618e
Rommel Rico <me@rommelrico.com>
2019-01-13 17:57:33 -0800

    Temporary code

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route } from "react-router-dom";
 
+// Temp
+const PageOne = () => {
+    return <div>Page One</div>;
+};
+const PageTwo = () => {
+    return <div>Page Two</div>;
+};
+
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div>
+            
+        </div>
+    );
 };
 
 export default App;
