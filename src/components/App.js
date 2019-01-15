cbbf57cb2a9b695e77ff989564e88c01aec94e3a
Rommel Rico <me@rommelrico.com>
2019-01-14 19:58:07 -0800

    Path to StreamList

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ const App = () => {
         <div>
             <BrowserRouter>
                 <div>
-
+                    <Route path="/" exact component={StreamList} />
                 </div>
             </BrowserRouter>
         </div>
