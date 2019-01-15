1bd47f853b5a2579f0754e3824f814df85957a9f
Rommel Rico <me@rommelrico.com>
2019-01-14 20:11:10 -0800

    Adding Links to the Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div className="ui secondary pointing menu">
-            <div className="right menu">
+            <Link to="/" className="item">
+                Streamer
+            </Link>
 
+            <div className="right menu">
+                <Link to="/" className="item">
+                    All Streams
+                </Link>
             </div>
         </div>
     );
