c7427b8eeaa96e68dffa68618bbad5e47f351c9e
Rommel Rico <me@rommelrico.com>
2019-01-20 19:38:13 -0800

    Showing validation errors

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -3,11 +3,13 @@ import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
 
-    renderInput({ input, label }) {
+    renderInput({ input, label, meta }) {
+
         return (
             <div className="field">
                 <label>{label}</label>
                 <input {...input} />
+                <div>{meta.error}</div>
             </div>
         );
     }
