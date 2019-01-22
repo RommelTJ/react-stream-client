ba632ece768e5ab569a7ada5f8a4ea198259ed10
Rommel Rico <me@rommelrico.com>
2019-01-22 07:11:29 -0800

    deleteStream action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -41,3 +41,8 @@ export const editStream = (id, formValues) => async dispatch => {
     const response = await streams.put(`/streams/${id}`, formValues);
     dispatch({ type: EDIT_STREAM, payload: response.data });
 };
+
+export const deleteStream = (id) => async dispatch => {
+    await streams.delete(`/streams/${id}`);
+    dispatch({ type: DELETE_STREAM, payload: id });
+};
