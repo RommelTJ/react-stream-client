38aadfcbde3302be5f70cd67f9d0136c6abc3b90
Rommel Rico <me@rommelrico.com>
2019-01-22 07:07:49 -0800

    fetchStream action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -31,3 +31,8 @@ export const fetchStreams = () => async dispatch => {
     const response = await streams.get('/streams');
     dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
+
+export const fetchStream = (id) => async dispatch => {
+    const response = await streams.get(`/stream/${id}`);
+    dispatch({ type: FETCH_STREAM, payload: response.data });
+};
