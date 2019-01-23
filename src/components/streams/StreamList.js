1709810972d740eff4d9352b6957c359fb2a9be2
Rommel Rico <me@rommelrico.com>
2019-01-23 06:35:27 -0800

    Refactoring StreamList to a class-based component

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -2,8 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from "../../actions";
 
-const StreamList = () => {
-    return <div>StreamList</div>;
-};
+class StreamList extends React.Component {
+
+    render() {
+        return (
+            <div>
+                StreamList
+            </div>
+        );
+    }
+    
+}
 
 export default connect(null, { fetchStreams })(StreamList);
