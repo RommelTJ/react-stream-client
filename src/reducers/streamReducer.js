6f94feb133f871a4dd134b8bc9dc5da58a74b4d4
Rommel Rico <me@rommelrico.com>
2019-01-22 21:21:56 -0800

    Initial streamReducer

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,14 @@
+import {
+    FETCH_STREAM,
+    FETCH_STREAMS,
+    CREATE_STREAM,
+    EDIT_STREAM,
+    DELETE_STREAM
+} from '../actions/types';
+
+export default (state = {}, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
