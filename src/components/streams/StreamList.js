2b9dbf66abdad5d4879f9ef4c0a6a42ada425de6
Rommel Rico <me@rommelrico.com>
2019-01-23 20:33:40 -0800

    Adding a new isSignedIn prop inside StreamList

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -41,6 +41,10 @@ class StreamList extends React.Component {
         });
     }
 
+    renderCreate() {
+
+    }
+
     render() {
         return (
             <div>
@@ -57,7 +61,8 @@ class StreamList extends React.Component {
 const mapStateToProps = (state) => {
     return {
         streams: Object.values(state.streams),
-        currentUserId: state.auth.userId
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn
     };
 };
 
