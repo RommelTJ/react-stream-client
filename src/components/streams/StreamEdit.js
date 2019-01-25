bfccb99086d90d08ed7508fe18b5f8fa9b2a782e
Rommel Rico <me@rommelrico.com>
2019-01-24 19:40:35 -0800

    Setting up initialValues from the props

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -23,7 +23,7 @@ class StreamEdit extends React.Component {
             <div>
                 <h3>Edit a Stream</h3>
                 <StreamForm
-                    initialValues={{ title: "EDIT ME", description: "CHANGE ME" }}
+                    initialValues={this.props.stream}
                     onSubmit={this.onSubmit}
                 />
             </div>);
