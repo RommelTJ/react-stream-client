c4aeacbc33122895610fab8a91754f1fd298c24e
Rommel Rico <me@rommelrico.com>
2019-01-24 19:36:10 -0800

    Refactoring StreamEdit

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from "../../actions";
+import { editStream, fetchStream } from "../../actions";
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
 
@@ -9,13 +10,20 @@ class StreamEdit extends React.Component {
         this.props.fetchStream(id);
     }
 
+    onSubmit = (formProps) => {
+        console.log(formProps);
+    };
+
     render() {
         if (!this.props.stream) {
             return <div>Loading...</div>;
         }
 
-        const title = this.props.stream.title;
-        return <div>{title}</div>;
+        return (
+            <div>
+                <h3>Edit a Stream</h3>
+                <StreamForm onSubmit={this.onSubmit} />
+            </div>);
     }
 
 }
@@ -24,4 +32,4 @@ const mapStateToProps = (state, ownProps) => {
     return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { editStream, fetchStream })(StreamEdit);
