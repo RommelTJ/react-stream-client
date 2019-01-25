a8931bd2474565cc440970e85e8275a68ce9e6a3
Rommel Rico <me@rommelrico.com>
2019-01-24 18:59:52 -0800

    scaffolding for selecting records from state

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const StreamEdit = (props) => {
     return <div>StreamEdit</div>;
+};
+
+const mapStateToProps = (state, ownProps) => {
+    return { stream: null };
 };
 
 export default StreamEdit;
