5c9715666469b2dcf5b85abd3889197ecad35533
Rommel Rico <me@rommelrico.com>
2019-01-26 09:49:55 -0800

    CSS fix

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -8,7 +8,7 @@ const Modal = (props) => {
                 <div className="header">Delete Stream</div>
                 <div className="content">Are you sure you want to delete this stream?</div>
                 <div className="actions">
-                    <button className="ui primary">Delete</button>
+                    <button className="ui primary button">Delete</button>
                     <button className="ui button">Cancel</button>
                 </div>
             </div>
