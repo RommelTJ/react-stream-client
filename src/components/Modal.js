79d4cbb3878b53c8223cc58bc5f69dd172bef9b7
Rommel Rico <me@rommelrico.com>
2019-01-26 09:49:34 -0800

    Adding modal actions

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,6 +7,10 @@ const Modal = (props) => {
             <div className="ui standard modal visible active">
                 <div className="header">Delete Stream</div>
                 <div className="content">Are you sure you want to delete this stream?</div>
+                <div className="actions">
+                    <button className="ui primary">Delete</button>
+                    <button className="ui button">Cancel</button>
+                </div>
             </div>
         </div>,
         document.querySelector("#modal")
