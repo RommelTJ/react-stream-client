1ae7aa094cc27c11689193212b1bcadac7167071
Rommel Rico <me@rommelrico.com>
2019-01-30 07:09:20 -0800

    Setting up the flv player

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -13,6 +13,12 @@ class StreamShow extends React.Component {
     componentDidMount() {
         const id = this.props.match.params.id;
         this.props.fetchStream(id);
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
     }
 
     render() {
